6ba6f8286c6d5683b0f561517543eb940d855b98
Dan Bikle <bikle@bikle.com>
2011-02-28 21:15:05 +0000

    app/cj/predictions/fx_past/index.bash

---
+++ app/cj/predictions/fx_past/index.bash
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# index.bash
+
+# I use this script to generate a list of a-tags like this:
+# Week: 2011-02-20 Through 2011-02-25 
+# Each a-tag will take me to a set of reports for a specific week.
+# The reports will show:
+# - Action (buy or sell) summaries
+# - pair summaries
+# - Details of all predictions
+
+exit 0
+
