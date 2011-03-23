05ba4acc4e507dc3cbcf3171b8a2f250cd85ffab
Dan Bikle <bikle@bikle.com>
2011-03-23 21:19:19 +0000

    run_fx_past_week.rb

--- app/cj/predictions/fx_past/run_fx_past_week.rb
+++ app/cj/predictions/fx_past/run_fx_past_week.rb
@@ -2,22 +2,31 @@
 # run_fx_past_week.rb
 #
 
-# I use this script to loop through .lst files created by sqlplus.
+# I use this script to loop through spool files created by sqlplus.
 
 require 'rubygems'
 require 'hpricot'
-require 'ruby-debug'
+# require 'ruby-debug'
 
+# I start by getting a list of spool files created by sqlplus:
 glb = Dir.glob("/pt/s/rl/bikle100/app/cj/predictions/fx_past/tmp_fx_past_week_20*.lst").sort
-# p glb
+
 glb.each{|fn|
-  debugger
+  # For each file, make note of the date embedded in the filename.
+  # The date should be a Sunday.
+  # I use the date to identify a weeks worth of data:
   the_date=fn.sub(/tmp_fx_past_week_/,'').sub(/.lst/,'').gsub(/-/,'_').sub(/\/.*\//,'')
+
+  # Next, I feed the file to Hpricot so I can access HTML in the file:
   doc = open(fn){ |f| Hpricot(f) }
+
+  # Load some html into a string:
   some_html=doc.search("table.table_fx_past_week").to_html
+
   # I want a file for this URL pattern:
   # href="/predictions/fx_past_wk2011_01_30"
   html_f=File.new("/pt/s/rl/bikle100/app/views/predictions/fx_past_wk#{the_date}.html.erb", "w")
+  # Fill the file with HTML which I had obtained from sqlplus:
   html_f.puts some_html
   p "#{html_f.path} File written"
   html_f.close
