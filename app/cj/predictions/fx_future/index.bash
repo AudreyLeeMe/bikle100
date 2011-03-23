25c7d10db67cb2ce1e714db709a5c54cf15349b5
Dan Bikle <bikle@bikle.com>
2011-03-23 23:09:22 +0000

    app/cj/predictions/fx_future/index.bash

--- app/cj/predictions/fx_future/index.bash
+++ app/cj/predictions/fx_future/index.bash
@@ -2,6 +2,8 @@
 
 # index.bash
 
-# I use this script to help me see Forex future predictions
+# I use this script to help me see Forex future predictions.
+
+echo hello >> /pt/s/rl/bikle100/app/views/predictions/_fx_future_spool.html.erb
 
 exit 0
