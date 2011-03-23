2533afa14e38cdc4659e724ac28d855bf248262a
Dan Bikle <bikle@bikle.com>
2011-03-23 20:37:31 +0000

    run_fx_past_week.rb

--- app/cj/predictions/fx_past/run_fx_past_week.rb
+++ app/cj/predictions/fx_past/run_fx_past_week.rb
@@ -9,5 +9,16 @@ require 'hpricot'
 require 'ruby-debug'
 
 glb = Dir.glob("/pt/s/rl/bikle100/app/cj/predictions/fx_past/tmp_fx_past_week_20*.lst").sort
-debugger
-p glb
+# p glb
+glb.each{|fn|
+  debugger
+  the_date=fn.sub(/tmp_fx_past_week_/,'').sub(/.lst/,'').gsub(/-/,'_').sub(/\/.*\//,'')
+  doc = open(fn){ |f| Hpricot(f) }
+  some_html=doc.search("table.table_fx_past_week").to_html
+  # I want a file for this URL pattern:
+  # href="/predictions/fx_past_wk2011_01_30"
+  html_f=File.new("/pt/s/rl/bikle100/app/views/predictions/fx_past_wk#{the_date}.html.erb", "w")
+  html_f.puts some_html
+  p "#{html_f.path} File written"
+  html_f.close
+}
