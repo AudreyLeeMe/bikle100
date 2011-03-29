5c4baf8b5a36ded07b56a9105f3d9e26ba703a26
Dan Bikle <bikle@bikle.com>
2011-03-29 12:42:07 +0100

    /pt/s/rl/bikle100/app/cj/html.bash

--- app/cj/html.bash
+++ app/cj/html.bash
@@ -3,3 +3,9 @@
 # html.bash
 
 # I use this script to generate views files (HTML mostly).
+
+cd /pt/s/rl/bikle100/app/cj/
+./predictions/fx_past/index.bash
+./predictions/us_stk_past/index.bash
+
+exit 0
