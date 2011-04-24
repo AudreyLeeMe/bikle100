f1c2e522122aa9465cd766df690eb3a93808cb01
Audrey Lee <audreyleemee@yahoo.com>
2011-04-24 06:48:32 +0100

    app/cj/predictions/fx_past/fx_past_week.sql

--- app/cj/predictions/fx_past/fx_past.sql
+++ app/cj/predictions/fx_past/fx_past.sql
@@ -20,6 +20,9 @@ pair
 ,prdate
 ,(LEAD(clse,12*1,NULL)OVER(PARTITION BY pair ORDER BY ydate)-clse)/clse g1
 ,(LEAD(clse,12*6,NULL)OVER(PARTITION BY pair ORDER BY ydate)-clse)/clse g6
+,clse price_0hr
+,LEAD(clse,12*1,NULL)OVER(PARTITION BY pair ORDER BY ydate) price_1hr
+,LEAD(clse,12*6,NULL)OVER(PARTITION BY pair ORDER BY ydate) price_6hr
 FROM di5min
 WHERE ydate > '2011-01-30'
 AND clse > 0
@@ -39,6 +42,9 @@ m.pair
 ,ROUND(l.score-s.score,2) rscore_diff2
 ,m.g1
 ,m.g6
+,price_0hr
+,price_1hr
+,price_6hr
 ,m.g6-m.g1 g5
 ,CORR(l.score-s.score,g6)OVER(PARTITION BY l.pair ORDER BY l.ydate ROWS BETWEEN 12*24*1 PRECEDING AND CURRENT ROW)rnng_crr1
 FROM svm62scores l,svm62scores s,fxpst10 m
--- app/cj/predictions/fx_past/fx_past_week.sql
+++ app/cj/predictions/fx_past/fx_past_week.sql
@@ -10,19 +10,19 @@
 
 -- Start by showing summarized data for each pair:
 
-COLUMN pair             FORMAT A8  HEADING     'Currency|Pair'    JUSTIFY left
-COLUMN avg_danbot_score FORMAT 9.99 HEADING    'Avg|DanBot|Score' JUSTIFY left
-COLUMN sharpe_ratio     FORMAT 9999.99 HEADING 'Sharpe|Ratio'     JUSTIFY left
-COLUMN avg_5hr_gain   FORMAT 99.9999 HEADING 'Avg of|5hr|gains'   JUSTIFY left
-COLUMN position_count FORMAT 9999  HEADING 'Count of|positions'   JUSTIFY left
-COLUMN sum_5hr_gain   FORMAT 99.9999 HEADING 'Sum of|5hr|gains'   JUSTIFY center
-COLUMN min_5hr_gain   FORMAT 99.9999 HEADING 'Min of|of|5hr|gains'JUSTIFY center
-COLUMN max_5hr_gain   FORMAT 99.9999 HEADING 'Max of|5hr|gains'   JUSTIFY right
-COLUMN stddev_5hr_gain FORMAT 99.9999 HEADING 'StdDeviation|of|5hr|gains' JUSTIFY right
+COLUMN pair             FORMAT A8  HEADING     'Currency|Pair'    
+COLUMN avg_danbot_score FORMAT 9.99 HEADING    'Avg|DanBot|Score' 
+COLUMN sharpe_ratio     FORMAT 9999.99 HEADING 'Sharpe|Ratio'     
+COLUMN avg_5hr_n_gain   FORMAT 99.9999 HEADING 'Avg of|5hr gains'   
+COLUMN position_count   FORMAT 9999  HEADING   'Count of|positions'   
+COLUMN sum_5hr_n_gain   FORMAT 99.9999 HEADING 'Sum of|5hr gains'   
+COLUMN min_5hr_n_gain   FORMAT 99.9999 HEADING 'Min of|of|5hr gains'
+COLUMN max_5hr_n_gain   FORMAT 99.9999 HEADING 'Max of|5hr gains'   
+COLUMN stddev_5hr_n_gain FORMAT 99.9999 HEADING'Standard|Deviation|of 5hr gains'
 
 BREAK ON REPORT
 
-COMPUTE SUM LABEL 'Sum:' OF sum_5hr_gain   ON REPORT
+COMPUTE SUM LABEL 'Sum:' OF sum_5hr_n_gain   ON REPORT
 COMPUTE SUM LABEL 'Sum:' OF position_count ON REPORT
 
 SET TIME off TIMING off ECHO off PAGESIZE 123 LINESIZE 188
@@ -33,12 +33,12 @@ SELECT
 pair
 ,ROUND(AVG(score_diff),2) avg_danbot_score
 ,ROUND(AVG(g5) / STDDEV(g5),2) sharpe_ratio
-,ROUND(AVG(g5),4)   avg_5hr_gain
+,ROUND(AVG(g5),4)   avg_5hr_n_gain
 ,COUNT(g5)          position_count
-,ROUND(SUM(g5),4)   sum_5hr_gain
-,ROUND(MIN(g5),4)   min_5hr_gain
-,ROUND(MAX(g5),4)   max_5hr_gain
-,ROUND(STDDEV(g5),4)stddev_5hr_gain
+,ROUND(SUM(g5),4)   sum_5hr_n_gain
+,ROUND(MIN(g5),4)   min_5hr_n_gain
+,ROUND(MAX(g5),4)   max_5hr_n_gain
+,ROUND(STDDEV(g5),4)stddev_5hr_n_gain
 FROM fxpst12
 WHERE rnng_crr1 > 0.0
 AND score_diff < -0.55
@@ -54,12 +54,12 @@ SELECT
 pair
 ,ROUND(AVG(score_diff),2) avg_danbot_score
 ,ROUND(AVG(g5) / STDDEV(g5),2) sharpe_ratio
-,ROUND(AVG(g5),4)   avg_5hr_gain
+,ROUND(AVG(g5),4)   avg_5hr_n_gain
 ,COUNT(g5)          position_count
-,ROUND(SUM(g5),4)   sum_5hr_gain
-,ROUND(MIN(g5),4)   min_5hr_gain
-,ROUND(MAX(g5),4)   max_5hr_gain
-,ROUND(STDDEV(g5),4)stddev_5hr_gain
+,ROUND(SUM(g5),4)   sum_5hr_n_gain
+,ROUND(MIN(g5),4)   min_5hr_n_gain
+,ROUND(MAX(g5),4)   max_5hr_n_gain
+,ROUND(STDDEV(g5),4)stddev_5hr_n_gain
 FROM fxpst12
 WHERE rnng_crr1 > 0.0
 AND score_diff > 0.55
@@ -70,6 +70,60 @@ GROUP BY pair
 HAVING(STDDEV(g5) > 0)
 ORDER BY pair
 /
+
+COLUMN anote FORMAT A120 HEADING 'Note:'
+
+SELECT
+'The above tables are summaries of predictions. A list of all predictions is displayed below '||
+'if you want to load it into a spreadsheet.' anote
+FROM dual
+/
+
+
+COLUMN timestamp_0hr FORMAT A11   HEADING 'Timestamp|at hour 0' 
+COLUMN danbot_score FORMAT 9.99   HEADING 'DanBot|Score|at hour 0' 
+COLUMN price_0hr    FORMAT 999.9999 HEADING 'Price at|hour 0'
+COLUMN price_1hr    FORMAT 999.9999 HEADING 'Price after|1 hour'
+COLUMN price_6hr    FORMAT 999.9999 HEADING 'Price after|6 hours'
+COLUMN gain_6hr1hr  FORMAT  99.9999 HEADING '5 hour gain|between|hr1 and hr6'
+COLUMN normalized_gain_5hr FORMAT 9.9999 HEADING 'Normalized|5hr gain'
+
+SELECT
+pair
+,ydate timestamp_0hr
+,ROUND(score_diff,4) danbot_score
+,ROUND(price_0hr,4)  price_0hr
+,ROUND(price_1hr,4)  price_1hr
+,ROUND(price_6hr,4)  price_6hr
+,ROUND(price_6hr-price_1hr,4)gain_6hr1hr
+,ROUND((price_6hr-price_1hr)/price_0hr,4)normalized_gain_5hr
+FROM fxpst12
+WHERE rnng_crr1 > 0.0
+AND score_diff < -0.55
+AND ydate > '&1'
+AND ydate - 7 < '&1'
+AND g1 > -0.0004
+ORDER BY pair,ydate
+/
+
+SELECT
+pair
+,ydate timestamp_0hr
+,ROUND(score_diff,4) danbot_score
+,ROUND(price_0hr,4)  price_0hr
+,ROUND(price_1hr,4)  price_1hr
+,ROUND(price_6hr,4)  price_6hr
+,ROUND(price_6hr-price_1hr,4)gain_6hr1hr
+,ROUND((price_6hr-price_1hr)/price_0hr,4)normalized_gain_5hr
+FROM fxpst12
+WHERE rnng_crr1 > 0.0
+AND score_diff > 0.55
+AND ydate > '&1'
+AND ydate - 7 < '&1'
+AND g1 < 0.0004
+ORDER BY pair,ydate
+/
+
 
 SPOOL OFF
 SET MARKUP HTML OFF
